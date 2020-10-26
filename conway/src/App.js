ad089406c62d7ca2ed72fe87bb238e5e52683c67
DJ Coleman <DJ@DJs-MacBook-Pro.attlocal.net>
2020-10-25 19:12:40 -0700

    changed code

--- conway/src/App.js
+++ conway/src/App.js
@@ -159,10 +159,13 @@ const App = () => {
 
       <h3>About this Algo </h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quos
-        architecto nesciunt repellat natus? Repellat dolores sunt aspernatur
-        totam quasi molestias et repellendus nihil voluptate suscipit quaerat
-        ea, minima doloremque?
+        The Game of Life, also known simply as Life, is a cellular automaton
+        devised by the British mathematician John Horton Conway in 1970. It is a
+        zero-player game, meaning that its evolution is determined by its
+        initial state, requiring no further input. One interacts with the Game
+        of Life by creating an initial configuration and observing how it
+        evolves. It is Turing complete and can simulate a universal constructor
+        or any other Turing machine.
       </p>
     </div>
   );
